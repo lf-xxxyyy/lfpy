b133c1426e9900fd5192045d13dfac35601e4e89
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-05 19:59:13 +0800

    if confition learning

---
+++ testCondition.py
@@ -0,0 +1,18 @@
+age = 20
+if age > 18:
+	print ('your age is %s' % age)
+	print ('adult')
+
+age = 3
+if age >=18 :
+	print ('adult')
+elif age >=6:
+	print ('teenager')
+else:
+	print ('kid')
+
+birth = input ('birth:')
+if birth > 2000:
+	print('00hou')
+else:
+	print('00qian')
