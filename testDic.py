8c824f784cfbdd67c55adff9d6e8adf434bd4331
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-06 22:59:21 +0800

    query value from dict

--- testDic.py
+++ testDic.py
@@ -1,3 +1,13 @@
 d = {'michael':95, 'bob':75, 'tracy':85}
 print(d['michael'])
 print(d['bob'])
+
+d['adam'] = 67
+print(d)
+
+
+print ('michael' in d )
+print ('tony' in d )
+
+print(d.get('bob', -1))
+print (d.get('tony', -1))
