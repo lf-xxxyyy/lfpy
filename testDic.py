509511e1e0180380251090e2059a1787d4234869
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-06 23:17:55 +0800

    set

--- testDic.py
+++ testDic.py
@@ -11,3 +11,18 @@ print ('tony' in d )
 
 print(d.get('bob', -1))
 print (d.get('tony', -1))
+
+
+s = set([5, 6, 7, 7, 1, 2, 3, 4, 4])
+print (s)
+
+s.add(10)
+print(s)
+
+s.remove(7)
+print(s)
+
+s2 = set([1, 4, 11])
+print (s & s2)
+
+print (s | s2)
