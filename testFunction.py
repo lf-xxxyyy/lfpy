0fed45ab287fb4c5414a16e065187f32e66b0394
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-09 00:02:04 +0800

    use type check

--- testFunction.py
+++ testFunction.py
@@ -1,24 +1,9 @@
-print(abs(100))
-print(abs(-20))
-
-print(int("123"))
-print(int(124))
-
-print(hex(255))
-print(hex(1000))
-
-
 def my_abs(x):
+
+	if not isinstance(x, (int, float)) :
+		raise TypeError('bad operand type')
+
 	if x>=0:
 		return x; 
 	else :
 		return -x;
-
-print ("use my abs_function val=%d" % my_abs(-100))
-
-
-
-def nop() :
-	pass
-
-print(nop())
--- testFunction2.py
+++ testFunction2.py
@@ -1,3 +1,5 @@
 from testFunction import my_abs
 
+print (my_abs(100))
+
 print(my_abs(-200))
