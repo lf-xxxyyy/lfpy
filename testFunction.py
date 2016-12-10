1ded06223fbdf5b14e883f7a45014882a0276a2a
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-10 15:15:05 +0800

    variable

--- testFunction.py
+++ testFunction.py
@@ -12,7 +12,30 @@ def my_abs(x):
 def my_hello(x):
 	return 'hello %s' % x
 
-def power(x) :
-	if not isinstance(x, (int, float)) :
+
+def power(x, n=2):
+	if not isinstance(x, (int, float)):
 		raise TypeError('bad operand type')
-	return x * x
+	if not isinstance(n, (int)) :
+		raise TypeError('bad operand type')
+
+	s = 1 
+	while n > 0 :
+		s = s * x
+		n = n - 1
+	return s
+
+def enroll(name, gender) :
+	print ('name:', name)
+	print ('gender:', gender )
+
+
+def add_end(L=[]) :
+	L.append("END")
+	return L
+
+def calc(numbers):
+	sum = 0
+	for num in numbers:
+		sum = sum + num * num
+	return sum
--- testFunction2.py
+++ testFunction2.py
@@ -1,6 +1,9 @@
 from testFunction import my_abs
 from testFunction import my_hello
 from testFunction import power
+from testFunction import enroll
+from testFunction import add_end
+from testFunction import calc
 
 
 print(my_hello('tony'))
@@ -9,4 +12,14 @@ print (my_abs(100))
 
 print(my_abs(-200))
 
-print(power(100))
+print (power(14))
+
+print(power(10, 4))
+
+enroll('tony', "male")
+
+print (add_end())
+
+print (calc((1, 2, 3)))
+
+print(calc(3,4,5))
