e9ee8941bdc39ceda8be49679a666fd4ffcf4c8f
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-10 23:04:32 +0800

    learn generator

--- testIterGernerator.py
+++ testIterGernerator.py
@@ -17,3 +17,57 @@ for k, v in d.items():
 	print (k, '=', v)
 
 
+
+g = (x * x for x in range(10))
+
+print (next(g))
+print (next(g))
+
+
+for n in g : 
+	print (n)
+
+
+for n in (x * x for x in range(10)):
+	print (n)
+
+
+def fib(max):
+	n, a, b = 0, 0, 1
+	while n < max : 
+		yield b
+		a, b = b, a +b
+		n = n + 1
+
+g = fib(10)
+
+print (next(g))
+print (next(g))
+print (next(g))
+print (next(g))
+print (next(g))
+
+
+
+def odd() :
+	print('step 1')
+	yield 1
+	print ('step 2')
+	yield 3
+	print ('step 3')
+	yield 5
+	return 'haha'
+
+
+o = odd()
+print (next(o))
+print (next(o))
+print (next(o))
+print (next(o))
+
+
+
+
+
+
+
