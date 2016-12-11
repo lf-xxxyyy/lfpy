4a85614558c761d2c46c96821aa5c5894ebf5c59
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-11 23:08:47 +0800

    test iterator

---
+++ testIterator.py
@@ -0,0 +1,19 @@
+from collections import Iterable
+
+print (isinstance([], Iterable))
+
+print (isinstance({}, Iterable))
+
+print (isinstance((x * x for x in range(10)), Iterable))
+
+print (isinstance(10, Iterable))
+
+M = {'a':'x', 'b':'y'}
+for k, v in M.items():
+	print (k, '=', v)
+
+def add(x, y , f):
+	return f(x) + f(y)
+
+
+print (add(-5, 6, abs))
