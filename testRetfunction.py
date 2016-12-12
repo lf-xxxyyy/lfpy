f475dded7509cd354f115479a66f369156c8c8fd
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-12 23:43:25 +0800

    add ret function

---
+++ testRetfunction.py
@@ -0,0 +1,15 @@
+
+def lazy_sum(*args):
+	def sum():
+		ax = 0
+		for n in args:
+			ax = ax + n
+		return ax
+	return sum
+
+f = lazy_sum(1,2,3,4,5,6)
+
+print(f)
+print(f())
+
+
--- testSort.py
+++ testSort.py
@@ -16,3 +16,5 @@ print(sorted(['bob', 'about', 'Zoo', 'Credit'], key = str.lower))
 print(sorted(['bob', 'about', 'Zoo', 'Credit'], key = str.lower, reverse=True))
 
 
+
+
