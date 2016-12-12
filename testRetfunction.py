bb2672035897f4ea7a9599c3f8a094f6a414d916
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-12 23:47:54 +0800

    define my ret function

--- testRetfunction.py
+++ testRetfunction.py
@@ -13,3 +13,28 @@ print(f)
 print(f())
 
 
+def my_lazy_sum(x, *args) :
+	if x > 0:
+		def sum():
+			ax = 0
+			for n in args:
+				ax = ax + n
+			return ax
+		return sum
+	else :
+		def pw_sum():
+			ax = 0 
+			for n in args:
+				ax = ax + n*n
+			return ax
+		return pw_sum
+
+
+f2 = my_lazy_sum(1, 1,2,3,4,5,6)
+print(f2)
+print(f2())
+
+
+f3 = my_lazy_sum(-1, 1,2,3,4,5,6,7)
+print (f3)
+print(f3())
