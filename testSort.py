fa59df72a6d059122854449a78c8d3bf6a4066b5
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-12 23:29:05 +0800

    add sort

---
+++ testSort.py
@@ -0,0 +1,14 @@
+print(sorted([36, 5, -12, 9, -21]))
+
+
+
+def my_power(x):
+	if x > 0 : 
+		return x
+	else :
+		return -x
+
+print(sorted([36, 5, -12, 9, -21], key = my_power))
+
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key = str.lower))
