05928166b3fa2875da8f05bc87b0c7d3992d2925
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-12 23:56:31 +0800

    define funcion in function

--- testRetfunction.py
+++ testRetfunction.py
@@ -38,3 +38,41 @@ print(f2())
 f3 = my_lazy_sum(-1, 1,2,3,4,5,6,7)
 print (f3)
 print(f3())
+
+
+def count() :
+	def f(i):
+		def g() :
+			return i * i
+		return g
+
+	fs = []
+	for n in range(1, 4):
+		fs.append(f(n))
+	return fs
+
+f4, f5, f6 = count()
+
+print(f4())
+print(f5())
+print(f6())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
