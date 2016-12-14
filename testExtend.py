1c32173f50f90fe043a49d0d42d284258cf839a6
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-14 23:04:38 +0800

    extend

--- testExtend.py
+++ testExtend.py
@@ -3,14 +3,44 @@ class Animal(object):
 		print('Animal is running...')
 
 class Dog(Animal):
-	pass
+	def run(self):
+		print('Dog is running...')
+
+	def eat(self):
+		print ('Eating meat...')
 
 class Cat(Animal):
-	pass
+	def run(self):
+		print('Cat is running...')
 
 
 dog = Dog()
 dog.run()
+dog.eat()
 
 cat = Cat()
 cat.run()
+
+a = list()
+b = Animal()
+c = Dog()
+
+print (isinstance(a, list))
+print (isinstance(b, Animal))
+print (isinstance(c, Animal))
+
+def run_twice(animal):
+	animal.run()
+	animal.run()
+
+run_twice(dog)
+
+run_twice(cat)
+
+class Tortoise(Animal):
+	def run(self):
+		print('Tortoise is running slowly...')
+
+run_twice(Tortoise())
+
+
