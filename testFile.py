cac193331250e33904ef0e5df8162c0df7e45a77
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-17 14:25:49 +0800

    nothing

---
+++ testFile.py
@@ -0,0 +1,11 @@
+try :
+	f = open('/Users/Tony/Documents/workspace/scrapy/lfpy/testFile.py', 'r')
+	print(f.read())
+finally:
+	if f:
+		f.close()
+
+
+with open('/Users/Tony/Documents/workspace/scrapy/lfpy/testFile.py', 'r') as f:
+	for line in f.readlines():
+		print(line.strip())
