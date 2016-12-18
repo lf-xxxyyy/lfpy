6552ab85078f8ed811ed33b845fc8eec20d56a4e
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-18 21:00:09 +0800

    serial

---
+++ testSerial.py
@@ -0,0 +1,18 @@
+import pickle
+
+d = dict(name='bob', age=20, score=88)
+
+print(pickle.dumps(d))
+
+f = open('dump.txt', 'wb')
+pickle.dump(d, f)
+f.close()
+
+
+d2 = dict(a = 'b')
+
+f2 = open('dump.txt', 'rb')
+d2 = pickle.load(f2)
+f2.close()
+
+print(d2)
