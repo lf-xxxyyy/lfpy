c4e8a5bba823a489f146ccdf3f31dcc03b9d1474
lf-xxxyyy <liufeng.lf@aliyun.com>
2016-12-19 23:01:55 +0800

    jsoning class

--- testSerial.py
+++ testSerial.py
@@ -22,3 +22,13 @@ d3 = dict(name ='bob', age = 20, score = 88)
 
 print(json.dumps(d3))
 
+class Student(object):
+	def __init__(self, name, age, score):
+		self.name = name
+		self.age = age
+		self.score = score
+
+s = Student('Bob', 20, 88)
+
+print(json.dumps(s, default=lambda obj: obj.__dict__))
+
