beaebcfffd2d0ed39f30591aef27d1f9a6392f4f
lf-xxxyyy <liufeng.lf@aliyun.com>
2017-01-01 21:26:47 +0800

    query from db

--- testSQL.py
+++ testSQL.py
@@ -9,8 +9,28 @@ cursor.execute('create table user( id varchar(20) primary key, name varchar(20))
 
 cursor.execute('insert into user(id, name) values(\'1\', \'michael\')')
 
+cursor.execute('insert into user(id, name) values(\'2\', \'janne\')')
+
 print(cursor.rowcount)
 
 cursor.close()
 conn.commit()
+conn.close()
+
+
+
+conn = sqlite3.connect('test.db')
+
+cursor = conn.cursor()
+
+cursor.execute('select * from user')
+
+values = cursor.fetchall()
+
+for aa in values : 
+	print (aa)
+
+
+
+cursor.close()
 conn.close()
