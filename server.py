7a8ba117574b54ee22efdcd4c0708d45188c2af4
lf-xxxyyy <liufeng.lf@aliyun.com>
2017-01-02 15:20:48 +0800

    server

---
+++ server.py
@@ -0,0 +1,7 @@
+from wsgiref.simple_server import make_server
+from testHtml import application
+
+httpd = make_server('', 8000, application)
+print('server http on port 8000...')
+
+httpd.serve_forever()
